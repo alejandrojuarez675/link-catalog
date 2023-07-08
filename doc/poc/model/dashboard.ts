d9e616d232f8d9b96803a40972574889341b6351
Alejandro juarez <alejandrojuarez675@gmail.com>
2023-07-08 17:52:53 -0300

    first commit

--- doc/poc/model/dashboard.ts
+++ doc/poc/model/dashboard.ts
@@ -1,3 +1,14 @@
 interface Dashboard {
+  id: String,
+  data: LinkNode,
+  privacy: "PUBLIC" | "RESTRICTED"
+}
 
+interface LinkNode {
+  id: String,
+  type: "MENU" | "LINK" | "FOLDER",
+  title: String,
+  description: String,
+  urlLogo: String | undefined,
+  nodes: LinkNode[],
 }
