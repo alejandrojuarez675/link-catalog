17d9d52b75cd3f524dc3aa1173475c11d80476db
Alejandro juarez <alejandrojuarez675@gmail.com>
2023-07-09 17:14:12 -0300

    link to demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'poc',
+    path: 'demo',
     loadChildren: () => import('./modules/poc/poc.module').then(x => x.PocModule)
   },
+  {
+    path: '**',
+    redirectTo: 'demo'
+  }
 ];
 
 @NgModule({
